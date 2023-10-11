bf200418ad8003da4f27f9e2e007ee2aac34b725
dadaPipes <markoliverstockflethstaf@gmail.com>
2023-10-11 19:42:47 +0200

    feat: Add responsive grid

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-const square = document.querySelector(".square");
-const gridSize = 16;
+const grid = document.querySelector(".grid");
+setElementSize(grid, 30, 30)
 
-for (let i = 0; i < gridSize; i++) {
-    const row = createElementWithClass("div", "square__row");
-    square.appendChild(row);
+let gridItemSize = 16;
+createGrid(gridItemSize);
 
-    for (let j = 0; j < gridSize; j++) {
-        const column = createElementWithClass("div", "square__column");
-        row.appendChild(column);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = createAndAppendElement("div", "grid__row", grid);
+    
+        for (let j = 0; j < size; j++) {
+            const column = createAndAppendElement("div", "grid__column", row);
+            column.addEventListener("mouseover", () => column.style.backgroundColor = "green");
+        }
+    }
+    setElementFraction("grid__column", "width", 1, size);
+    setElementFraction("grid__row", "height", 1, size);
+}
+
+function setElementSize(element, width, height) {
+    element.style.width = width + "em";
+    element.style.height = height + "em";
+}
+
+function createAndAppendElement(type, className, parent) {
+    const element = document.createElement(type);
+    element.className = className;
+    parent.appendChild(element);
+    return element;
+}
+
+function setElementFraction(className, property, numerator, denominator) {
+    const percentage = (numerator / denominator) * 100;
+    const elements = document.getElementsByClassName(className);
+    
+    for (let element of elements) {
+        element.style[property] = percentage + "%";
     }
 }
 
-function createElementWithClass(type, className) {
-    const element = document.createElement(type);
-    element.className = className;
-    return element;
+function setGridSize() {
+    let gridSize = validateGridNumber(prompt("Type a number between 10 and 100"));
+
+    // Clear the grid before creating a new one
+    grid.innerHTML = '';
+
+    // Create a new grid with the user-specified size
+    createGrid(gridSize);
+}
+
+function validateGridNumber(userInput) {
+    let gridNumber = Number(userInput);
+
+    while (isNaN(gridNumber) || gridNumber < 10 || gridNumber > 100) {
+        
+        if (isNaN(gridNumber)) {
+            userInput = prompt("That is not a valid number");
+        } else if (gridNumber < 10) {
+            userInput = prompt("Number must be 10 or higher");
+        } else {
+            userInput = prompt("Number must be 100 or lower");
+        }
+        gridNumber = Number(userInput);
+    }
+    
+    return gridNumber;
 }
